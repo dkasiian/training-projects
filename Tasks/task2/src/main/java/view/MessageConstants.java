b77402e9ca96407c5177700fa379cf038094139d
Dmytro Kasiianenko <dimkasiyan@gmail.com>
2019-05-11 16:43:34 +0300

    Added View class

---
+++ Tasks/task2/src/main/java/view/MessageConstants.java
@@ -0,0 +1,16 @@
+package view;
+
+/**
+ * Stores message constants for internationalization an application
+ */
+public interface MessageConstants {
+    String INPUT_MAKE = "input.make.choice";
+    String INPUT_MAKE_ACCESSORIES = "input.make.report";
+    String INPUT_WRONG_CHOICE = "input.wrong.choice";
+    String INPUT0_FINISH = "input.finish.choice";
+
+    String INPUT1_JAVA_DAY_KYIV = "input.choice.conference.java.kyiv";
+    String INPUT2_JAVA_DAY_LVIV= "input.choice.conference.java.lviv";
+    String INPUT3_JAVA_DAY_ODESA= "input.choice.conference.java.odesa";
+
+}
--- Tasks/task2/src/main/java/view/View.java
+++ Tasks/task2/src/main/java/view/View.java
@@ -5,9 +5,33 @@ import model.entities.Conference;
 import model.entities.Report;
 import model.entities.Rest;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class View {
+    private static String MESSAGES_BUNDLE_NAME = "messages";
+    private static final ResourceBundle bundle = ResourceBundle.getBundle(
+            MESSAGES_BUNDLE_NAME,
+//            new Locale("ua", "UA"));   // Ukrainian
+            new Locale("en"));        // English
+
+    /**
+     * Prints one message.
+     *
+     * @param message message to print
+     */
     public void printMessage(String message){
-        System.out.println(message);
+        System.out.println(bundle.getString(message));
+    }
+
+    /**
+     * Prints several messages.
+     *
+     * @param messages messages to print
+     */
+    public void printMessages(String ... messages){
+        for (String message : messages)
+            System.out.println(bundle.getString(message));
     }
 
     public void printConferenceInfo(Conference conference){
@@ -15,15 +39,26 @@ public class View {
 
         conferenceInfo.append("Reports: ");
         for (Report report : conference.getReports()){
-            conferenceInfo.append(report.getReportName() + ", " + report.getSpeakerName() + ", " + report.getText() + "; ");
+            conferenceInfo.append(report.getReportName())
+                    .append(", ")
+                    .append(report.getSpeakerName())
+                    .append(", ")
+                    .append(report.getText())
+                    .append("; ");
         }
         conferenceInfo.append("\nRests: ");
         for (Rest rest: conference.getRests()){
-            conferenceInfo.append(rest.getAfterReportName() + ", " + rest.getMinutes() + "; ");
+            conferenceInfo.append(rest.getAfterReportName())
+                    .append(", ")
+                    .append(rest.getMinutes())
+                    .append("; ");
         }
         conferenceInfo.append("\nCoffee breaks: ");
         for (CoffeeBreak coffeeBreak : conference.getCoffeeBreaks()){
-            conferenceInfo.append(coffeeBreak.getAfterReportName() + ", " + coffeeBreak.getCupsNumber() + "; ");
+            conferenceInfo.append(coffeeBreak.getAfterReportName())
+                    .append(", ")
+                    .append(coffeeBreak.getCupsNumber())
+                    .append("; ");
         }
 
         System.out.println(conferenceInfo);
